93401a073a404af143a622b6a9c9931fab893591
Paul-G2 <Paul-G2@users.noreply.github.com>
2023-03-23 22:26:48 -0400

    Added a Help dialog.

--- CopycatJS.js
+++ CopycatJS.js
@@ -604,6 +604,268 @@
 
 /**
  * @classdesc
+ * This class implements a draggable dalog box.
+ * 
+ */
+Namespace.Dialog = class 
+{   
+    /**
+     * @constructor
+     * 
+     * @param {HTMLElement} [parent=document.body] - The dialog's parent element.
+     * @param {Number} width - The dialog width, in vw units
+     * @param {Number} height - The dialog height, in vh units.
+     * @param {String} [title] - Text to be displayed on the dialog's title bar.
+     * @param {Boolean} [modal=false] - Whether the dialog is modal. (Currently 
+     *      only non-modal is supported.)
+     * @param {String} [bkgndColor] - The dialog's background color.
+     * @param {String} [titleBarColor] - The dialog's title bar color.
+     */
+    constructor(parent, width, height, title, modal, bkgndColor, titleBarColor)
+    {
+        this.parent        = parent || document.body;
+        this.width         = width;
+        this.height        = height;
+        this.modal         = modal; // Not supported yet
+        this.mainDiv       = null;
+        this.startPos      = [0,0];  
+        this.mouseDownLoc  = [0,0];  
+        this.dragStartFunc = this._onDragStart.bind(this);
+        this.dragMoveFunc  = this._onDragMove.bind(this);
+        this.dragEndFunc   = this._onDragEnd.bind(this);
+        this.onOk          = null;
+        this.onCancel      = null;
+        this.fontFamily    = 'verdana, arial, helvetica, sans-serif';
+
+        // Create the user interface
+        this._initUi( (title || ""), (bkgndColor || 'white'), 
+            (titleBarColor || bkgndColor || 'white') );      
+    }
+
+
+    /**
+     * Lays out the UI.
+     * @private
+     * 
+     * @param {String} [title] - Text to be displayed in the dialog's title bar.
+     * @param {String} [bkgndColor] - The dialog's background color.
+     * @param {String} [titleBarColor] - The dialog's title bar color.
+     * 
+     */
+    _initUi(title, bkgndColor, titleBarColor)
+    {
+        const UiUtils = Namespace.UiUtils;
+
+        // Container div
+        this.mainDiv = UiUtils.CreateElement('div', 'dialog_maindiv', this.parent); 
+        UiUtils.StyleElement(this.mainDiv, {display:'none', width:
+            this.width.toString() + 'vw', height:this.height.toString() + 'vh', 
+            left:((100 - this.width)/2).toString() + 'vw', 
+            top:((100 - this.height)/2).toString() + 'vh', 
+            zIndex:'10', backgroundColor:bkgndColor,
+            border:'1px solid black'} );
+            
+        // Titlebar 
+        const titleFontSize = (.03 * this.height).toString() + 'vh';
+        this.titleDiv = UiUtils.CreateElement('div', 'dialog_titlediv', this.mainDiv); 
+        this.titleDiv.innerHTML = title;
+        UiUtils.StyleElement(this.titleDiv, {
+            width:'95%',height:'5%',
+            left:'0px', top:'0px', display:'flex', alignItems:'center', justifyContent:'center',
+            backgroundColor:titleBarColor, fontSize:titleFontSize, fontFamily:this.fontFamily} 
+        );
+        this.titleDiv.className += ' noselect';
+        this.titleDiv.addEventListener( 
+            (UiUtils.isTouchDevice() ? 'touchstart' : 'mousedown'), this.dragStartFunc);  
+
+        // Close button
+        this.closeDiv = UiUtils.CreateElement('div', 'dialog_closediv', this.mainDiv); 
+        this.closeDiv.innerHTML = 'X';
+        UiUtils.StyleElement(this.closeDiv, {
+            width:'5%', height:'5%', 
+            right:'0px', top:'0px', display:'flex', alignItems:'center', justifyContent:'center',
+            backgroundColor:titleBarColor, fontSize:titleFontSize, fontFamily:this.fontFamily} 
+        );
+        this.closeDiv.className += ' noselect';
+        this.closeDiv.addEventListener( (UiUtils.isTouchDevice() ? 'touchstart' : 'click'), 
+            this._onTitleBarClose.bind(this) ); 
+        this.closeDiv.addEventListener( 
+            'mouseover', function() { this.closeDiv.style.fontWeight = 'bold'; }.bind(this) ); 
+        this.closeDiv.addEventListener( 
+            'mouseout',  function() { this.closeDiv.style.fontWeight = 'normal'; }.bind(this) ); 
+
+        // User content div
+        this.userDiv = UiUtils.CreateElement('div', 'dialog_userdiv', this.mainDiv); 
+        UiUtils.StyleElement(this.userDiv, {bottom:'0px', left:'0px', 
+            width:'100%', height:'95%'} ); 
+    }
+
+
+    /**
+     * Shows the dialog.
+     * 
+     * @param {Function} [onOk] - Function to invoke if the dialog's OK button is clicked.
+     * @param {Function} [onCancel] - Function to invoke if the dialog is cancelled.
+     * 
+     */
+    show(onOk, onCancel)
+    {
+        if (onOk) { this.onOk = onOk; }
+        if (onCancel) { this.onCancel = onCancel; }
+
+        this.mainDiv.style.display = 'inline-block';  
+    }
+
+
+    /**
+     * Hides the dialog.
+     * 
+     */
+    hide()
+    {
+        this.mainDiv.style.display = 'none';  
+    }
+
+
+    /**
+     * Indicates whether the dialog is currenty shown.
+     * 
+     */
+    isShown()
+    {
+        return this.mainDiv.style.display != 'none';  
+    }
+
+
+    /**
+     * Handler for clicks on the dismiss button. (Sub-classes can override.)
+     * @private
+     * 
+     */
+    _onTitleBarClose()
+    {
+        if (this.onCancel) { this.onCancel(this); }
+        this.hide();  
+    }
+
+
+    /**
+     * Handler for drag-start events.
+     * @private
+     * 
+     * @param {Event} event - Event info.
+     * 
+     */
+    _onDragStart(event)
+    {
+        const mouseLoc = this._getEventCoordinates(event);
+        if ( (mouseLoc[0] !== null) && (mouseLoc[1] !== null) ) 
+        { 
+            event.preventDefault();
+            
+            const rect = this.mainDiv.getBoundingClientRect();
+            this.startPos = {x:rect.left, y:rect.top};
+            this.mouseDownLoc = mouseLoc;
+
+            document.addEventListener( 
+                (Namespace.UiUtils.isTouchDevice() ? 'touchmove' : 'mousemove'), this.dragMoveFunc );  
+
+            document.addEventListener( 
+                (Namespace.UiUtils.isTouchDevice() ? 'touchend touchcancel' : 'mouseup'), this.dragEndFunc );  
+        }
+    }
+
+
+    /**
+     * Handler for drag-move events.
+     * @private
+     * 
+     * @param {Event} event - Event info.
+     * 
+     */
+    _onDragMove(event)
+    {
+        const mouseLoc = this._getEventCoordinates(event);
+        if ( (mouseLoc[0] !== null) && (mouseLoc[1] !== null) ) 
+        { 
+            event.preventDefault();
+            const w = parseFloat(this.mainDiv.style.width);
+            const pw = this.parent.clientWidth || 1024;
+            const ph = this.parent.clientHeight || 1024;
+            const newX = Math.max(64-w, Math.min(pw-64, this.startPos.x + mouseLoc[0]-this.mouseDownLoc[0]));
+            const newY = Math.max(0,    Math.min(ph-64, this.startPos.y + mouseLoc[1]-this.mouseDownLoc[1]));
+            this.mainDiv.style.left = newX.toString() + 'px';  
+            this.mainDiv.style.top  = newY.toString() + 'px';  
+        }
+    }
+
+
+    /**
+     * Handler for drag-end events.
+     * @private
+     * 
+     * @param {Event} event - Event info.
+     * 
+     */
+    // eslint-disable-next-line no-unused-vars
+    _onDragEnd(event)
+    {
+        document.removeEventListener( 
+            (Namespace.UiUtils.isTouchDevice() ? 'touchmove' : 'mousemove'), this.dragMoveFunc );  
+
+        document.removeEventListener( 
+            (Namespace.UiUtils.isTouchDevice() ? 'touchend touchcancel' : 'mouseup'), this.dragEndFunc );  
+    }
+
+
+    /**
+     * Gets the coordinates from a given event.
+     * @private
+     * 
+     * @param {Event} event - Event info.
+     * 
+     */
+    _getEventCoordinates(event)
+    {
+        // Read the event coordinates
+        let cx1 = event.clientX;
+        let cy1 = event.clientY;
+
+        if ( (typeof(cx1) == 'undefined') || (typeof(cy1) == 'undefined') || (cx1 === null) || (cy1 === null) ) {
+            if ( this.isTouchDevice && event.originalEvent && event.originalEvent.targetTouches ) { 
+                if (event.originalEvent.targetTouches.length > 0)  {
+                    cx1 = event.originalEvent.targetTouches[0].clientX;
+                    cy1 = event.originalEvent.targetTouches[0].clientY;
+                }
+            }
+        }
+
+        if ( (typeof(cx1) == 'undefined') || (typeof(cy1) == 'undefined') ) {
+            cx1 = cy1 = null;
+        }
+        return [cx1, cy1];
+    }
+
+};
+
+})( window.CopycatJS = window.CopycatJS || {} );
+
+
+
+
+
+
+
+
+
+
+// eslint-disable-next-line no-shadow-restricted-names, no-unused-vars, no-extra-semi
+;(function(Namespace, undefined) {
+    "use strict";
+
+
+/**
+ * @classdesc
  * A Bond is a relation between Letters and/or Groups in the same string. 
  * For example, a letter-successorship relation between 'a' and  
  * 'b' in the string 'abc.'
@@ -9005,6 +9267,86 @@ Namespace.GroupGraphic = class
 // eslint-disable-next-line no-shadow-restricted-names, no-unused-vars, no-extra-semi
 ;(function(Namespace, undefined) {
     "use strict";
+
+/**
+ * @classdesc
+ * This class implements a dialog for displaying help info.
+ * 
+ */
+Namespace.HelpDialog = class extends Namespace.Dialog
+{
+
+    /**
+     * @constructor
+     * 
+     * @param {CopycatUi} copycatUi - The parent Ui.
+     * @param {HTMLElement} parent - The html div that hosts
+     *  the dialog.
+     */
+    constructor(parent) 
+    {
+        super(parent, 65, 80, 'Copycat Help', false, '#d5bfa2', '#c09f72');
+        this._buildUi();
+    }
+
+
+    _buildUi()
+    {
+        this.textDiv = Namespace.UiUtils.CreateElement('div', 'text-div',
+            this.userDiv, {top:'3%', left:'3%', width:'94%', height:'94%',
+            fontSize:'1.7vh', fontFamily:this.fontFamily});
+
+        this.textDiv.innerHTML =
+        '<p>Copycat is a computer model of human analogy-making.</p>' + 
+        '<p>It tries to solve letter puzzles of the form "<b><i>abc</i></b> is to <b><i>abd</i></b> as <b><i>ijk</i></b> is to what?"</p>' +
+        '<p>You can enter puzzle strings in the green upper-left area, then click the <i>play</i> button to watch Copycat "think" about ' +
+        'a solution. You can also pause, single-step, reset, and adjust the speed of the thinking.</p>' +
+
+        '<p>Some interesting examples to try are:</p>' +
+        '<ul>' +
+        '<li> <b><i>abc</b></i> &#x2192; <b><i>abd</b></i>, &nbsp; <b><i>kji</b></i> &#x2192; ?</li>' +
+        '<li> <b><i>abc</b></i> &#x2192; <b><i>abd</b></i>, &nbsp; <b><i>iijjkk</b></i> &#x2192; ? </li>' +
+        '<li> <b><i>abc</b></i> &#x2192; <b><i>abd</b></i>, &nbsp; <b><i>mrrjjj</b></i> &#x2192; ?</li>' +
+        '</ul>' +
+
+        '<p>While it&rsquo;s running, Copycat displays the bonds it finds between adjacent letters (as blue lines), ' +
+        'the correspondences it finds between the initial and target strings (as purple jagged lines), ' + 
+        'groups that it thinks may be important (as green boxes), descriptions of all the letters ' +
+        '(gray text) and other information. A flashing graphic indicates a structure that Copycat is considering ' + 
+        ' but hasn&rsquo;t yet committed to.</p>' + 
+
+        '<p>The thermometer indicates how "happy" Copycat is with its current progress; lower temperatures ' + 
+        'imply greater happiness.</p>' +
+
+        '<p>In the yellow <i>Slipnet</i> area, Copycat&rsquo;s built-in concepts are shown in a grid. ' + 
+        'The size of the dot over each concept indicates how important that concept is to Copycat at the current moment.</p>' +
+
+        '<p>The pink <i>Coderack</i> area displays a list of the subroutines or "codelets" that Copycat ' +
+        'uses to perform its work. The number of each type of codelet currently in Copycat&rsquo;s stack in shown in a dynamical ' +
+        'bar graph.</p>' +
+
+        '<br/>' +
+        '<p>For (much) more information, check out the book <i>Fluid Concepts and Creative Analogies</i> by Douglas Hofstadter et. al.</p>';
+    }
+
+};
+
+})( window.CopycatJS = window.CopycatJS || {} );
+
+
+
+
+
+
+
+
+
+
+
+
+// eslint-disable-next-line no-shadow-restricted-names, no-unused-vars, no-extra-semi
+;(function(Namespace, undefined) {
+    "use strict";
     
     
 /**
@@ -10254,6 +10596,18 @@ Namespace.TopbarUi = class {
         ); 
         this.titleSpan.innerHTML = 'Copycat';
         this.titleSpan.className += " noselect";
+
+        this.helpBtn = UiUtils.CreateElement('button', 'help-btn',
+            this.mainDiv, {top:'22%', height:'56%', right:'2vh', width:'10%',
+            display:'flex', alignItems:'center', justifyContent:'center',
+            color:'#404040', fontFamily:'Arial', fontWeight:'normal',
+            fontSize: '3vh', background:'#dfdfdf', border:'1px solid #404040' }
+        );
+        this.helpBtn.innerHTML = 'Help';
+        this.helpBtn.className += " noselect";
+        this.helpBtn.onclick = this._onHelpBtnClick.bind(this);
+
+        this.helpDialog = new Namespace.HelpDialog(document.getElementById('app_area'));
     }
 
 
@@ -10277,6 +10631,24 @@ Namespace.TopbarUi = class {
     {    
         // Nothing to do here
     }
+
+
+    /**
+     * Handler for help-button clicks.
+     * @private
+     * 
+     */
+    _onHelpBtnClick()
+    {    
+        if (this.helpDialog.isShown()) {
+            this.helpDialog.hide();
+        }
+        else {
+            this.helpDialog.show();
+        }
+    }
+
+
 };
 
 
@@ -10361,6 +10733,18 @@ Namespace.UiUtils.StyleElement = function(elem, styles)
     if (!Object.prototype.hasOwnProperty.call(styles, 'padding'))  { elem.style.padding = '0px'; }
 
     return elem;
+};
+
+
+/**
+ * Tries to determine whether we're on a touch device.
+ * @static
+ * 
+ */
+Namespace.UiUtils.isTouchDevice = function()
+{
+    // This test is fallible, but it seems to be the best we can do.
+    return ( ('ontouchstart' in document.documentElement) || window.navigator.msMaxTouchPoints );
 };
 
 
@@ -10569,7 +10953,7 @@ Namespace.WorkspaceHeaderUi = class
         this.speedSlider = UiUtils.CreateElement('input', 'speed_slider',
             this.ctrlsDiv, {width:'70%', height:'10%', top:'60%', left:'15%',
             accentColor:this.fontColor},
-            {type:'range', min:1, max:100, value:50});
+            {type:'range', min:1, max:100, value:55});
         this.speedSlider.draggable = true;
         this.speedSlider.ondragstart = function(e) { 
             e.preventDefault(); 
