3d590bc21043ff6702c762ade8ef8070f913b221
Paul-G2 <Paul-G2@users.noreply.github.com>
2023-03-25 14:45:39 -0400

    Tweaked the Help text.

--- src/UI/HelpDialog.js
+++ src/UI/HelpDialog.js
@@ -38,8 +38,8 @@ Namespace.HelpDialog = class extends Namespace.Dialog
         '<p></p>' + 
         '<p>Copycat is a computer model of human analogy-making.</p>' + 
         '<p>It tries to solve letter puzzles of the form "<b><i>abc</i></b> is to <b><i>abd</i></b> as <b><i>ijk</i></b> is to what?"</p>' +
-        '<p>You can enter puzzle strings in the green upper-left area, then click the <i>play</i> button to watch Copycat "think" about ' +
-        'a solution. You can also pause, single-step, reset, and adjust the speed of the thinking.</p>' +
+        '<p>You can enter puzzle strings in the green upper-left area, then click the <i>play</i> button to watch Copycat "think." ' +  
+        'You can also pause, single-step, reset, and adjust the speed of the demo.</p>' +
 
         '<p>Some interesting examples to try are:</p>' +
         '<ul>' +
@@ -51,7 +51,7 @@ Namespace.HelpDialog = class extends Namespace.Dialog
         '<p>While it&rsquo;s running, Copycat displays the bonds it finds between adjacent letters (as blue lines), ' +
         'the correspondences it finds between the initial and target strings (as purple jagged lines), ' + 
         'groups that it thinks may be important (as green boxes), descriptions of all the letters ' +
-        '(gray text) and other information. A flashing graphic indicates a structure that Copycat is considering ' + 
+        '(gray text), and other information. A flashing graphic indicates a structure that Copycat is considering ' + 
         ' but hasn&rsquo;t yet committed to.</p>' + 
 
         '<p>The thermometer indicates how "happy" Copycat is with its current progress; lower temperatures ' + 
@@ -60,8 +60,8 @@ Namespace.HelpDialog = class extends Namespace.Dialog
         '<p>In the yellow <i>Slipnet</i> area, Copycat&rsquo;s built-in concepts are shown in a grid. ' + 
         'The size of the dot over each concept indicates how important that concept is to Copycat at the current moment.</p>' +
 
-        '<p>The pink <i>Coderack</i> area displays the list of the subroutines or "codelets" that Copycat ' +
-        'uses to perform its work. The number of each type of codelet currently in the coderack in shown in a dynamical ' +
+        '<p>The pink <i>Coderack</i> area displays the list of subroutines or "codelets" that Copycat ' +
+        'uses to perform its work. The number of each type of codelet currently in the coderack is shown in a dynamical ' +
         'bar graph. The last-run codelet is indicated by a black triangle.</p>' +
 
         '<p>For (much) more information, check out the book <i>Fluid Concepts and Creative Analogies</i> by Douglas Hofstadter et. al.</p>';
